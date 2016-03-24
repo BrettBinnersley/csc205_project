ec22ce6165b9e7d2f1734bf986f396fc085b3e1e
Brett Binnersley <Brettbinnersley14@gmail.com>
2016-03-24 12:47:54 -0700

    Foobar

--- Menu/MenuText.java
+++ Menu/MenuText.java
@@ -5,6 +5,9 @@ Renders the background of the scene (Grass)
 */
 
 import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 
 class MenuText extends GameObject {
 
@@ -13,20 +16,33 @@ class MenuText extends GameObject {
     SetImage("menuBackground", 0, 0);
     SetType(OBJECTTYPE.BACKGROUND);
     depth = 0;
-    // scene_width = width;
-    // scene_height = height;
+    font = new Font("Serif", Font.PLAIN, 96);
   }
 
   // Render the background
   @Override
   public void Render(Graphics2D canvas) {
-    // for (int y=0; y<scene_height; y+=256) {
-    //   for (int x=0; x<scene_width; x+=256) {
-    //     canvas.drawImage(image, x, y, null);
-    //   }
-    // }
+    canvas.setFont(font);
+    canvas.setColor(new Color(0, 0, 0));
+    FontMetrics metrics = canvas.getFontMetrics(font);
+
+    int strHeight = metrics.getHeight();
+    String messages[] = {
+      "Controls",
+      "Move: <WADS>",
+      "Shoot: <LMB>"
+    };
+
+    int drawX = Scene.Width() / 2;
+    int drawY = Scene.Height() / 2 - (messages.length * strHeight / 2) - 150;  // Bad hardcoded constants. BLEH
+    for (String msg : messages) {
+      drawY += (int)(strHeight * 1.2);
+      int strWidth = metrics.stringWidth(msg) / 2;
+      canvas.drawString(msg, drawX - strWidth, drawY - strHeight);
+    }
   }
 
   private int scene_width;
   private int scene_height;
+  private Font font;
 }
